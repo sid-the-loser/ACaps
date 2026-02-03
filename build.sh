c3e7429af45e9c1b549edebb1ee9e2bc58649c1a
Sidharth <sidharthsuresh07@gmail.com>
2026-02-03 00:00:43 -0500

    added more build support

--- build.sh
+++ build.sh
@@ -1,1 +1,8 @@
-go build -ldflags="-s -w" -o ./builds/acaps ./main.go
+GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o ./builds/linux_amd64/acaps ./main.go
+GOOS=linux GOARCH=arm64 go build -ldflags="-s -w" -o ./builds/linux_arm64/acaps ./main.go
+
+GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w" -o ./builds/macos_amd64/acaps ./main.go
+GOOS=darwin GOARCH=arm64 go build -ldflags="-s -w" -o ./builds/macos_arm64/acaps ./main.go
+
+GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o ./builds/win_amd64/acaps.exe ./main.go
+GOOS=windows GOARCH=arm64 go build -ldflags="-s -w" -o ./builds/win_arm64/acaps.exe ./main.go
